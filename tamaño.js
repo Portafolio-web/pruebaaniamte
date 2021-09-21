a0f1de7020a68d8e32f2c1508e35e0617765e7b8
Portafolio-web <78131157+Portafolio-web@users.noreply.github.com>
2021-09-21 16:03:21 -0500

    Add files via upload

--- tamaño.js
+++ tamaño.js
@@ -55,12 +55,12 @@ if (reversed == null) { reversed = false; }
 	this._renderFirstFrame();
 
 }).prototype = p = new lib.AnMovieClip();
-p.nominalBounds = new cjs.Rectangle(115.7,160.9,904.6999999999999,434.80000000000007);
+p.nominalBounds = new cjs.Rectangle(106.7,158.9,913.6999999999999,436.80000000000007);
 // library properties:
 lib.properties = {
 	id: 'E9BC3AF504AF3149B678E704B14FFE0D',
-	width: 868,
-	height: 484,
+	width: 850,
+	height: 480,
 	fps: 24,
 	color: "#FFFFFF",
 	opacity: 1.00,
